20f95fdfedcb6d35bbf5fd8f9de5059e1886fe59
andrew <andrew@littleligths.ai>
2020-03-24 09:00:07 +0800

    add auto hide

--- client/Assets/Scripts/Application/Windows/UUILevelUp.cs
+++ client/Assets/Scripts/Application/Windows/UUILevelUp.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using UnityEngine.UI;
 using UGameTools;
+using UnityEngine;
 
 namespace Windows
 {
@@ -13,19 +14,26 @@ namespace Windows
         protected override void InitModel()
         {
             base.InitModel();
-            ButtonClose.onClick.AddListener(() => { HideWindow(); });
-            Root.OnMouseClick((g) => { HideWindow(); });
-            //Write Code here
+            //ButtonClose.onClick.AddListener(() => { HideWindow(); });
+            //Root.OnMouseClick((g) => { HideWindow(); });
         }
+
+        private float hideTime = -1;
         protected override void OnShow()
         {
             base.OnShow();
+            hideTime = Time.time + 2f;
         }
         protected override void OnHide()
         {
             base.OnHide();
         }
 
+        protected override void OnUpdate()
+        {
+            base.OnUpdate();
+            if (hideTime < Time.time) HideWindow();
+        }
         internal void ShowWindow(int level)
         {
             lb_level.text = $"{level}";
